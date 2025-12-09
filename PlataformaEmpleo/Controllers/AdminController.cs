7b24191157b2e5b68a7054110b898a24a38874b0
Edisson Salgado <edileonarsalfado@gmail.com>
2025-12-08 20:19:05 -0600

    Limpieza: eliminar archivos ignorados y normalizar repo

---
+++ PlataformaEmpleo/Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlataformaEmpleo.Data; // ajusta si tu DbContext está en otro namespace
+
+namespace PlataformaEmpleo.Controllers
+{
+    [Authorize] // evita que usuarios anónimos accedan
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var vm = new AdminDashboardViewModel
+            {
+                TotalCandidatos = await _context.Candidato.CountAsync(),
+                TotalCVs = await _context.OfertaEmpleo.CountAsync(),                  
+                TotalOfertaEmpleos = await _context.Postulacion.CountAsync(),
+                TotalOfertaPostulaciones = await _context.OfertaPostulacion.CountAsync(),
+                TotalPostulaciones = await _context.CV.CountAsync(),
+                TotalReclutadores = await _context.Reclutador.CountAsync()
+            };
+
+            return View(vm);
+        }
+    }
+
+    // ViewModel interno simple 
+    public class AdminDashboardViewModel
+    {
+        public int TotalCandidatos { get; set; }
+        public int TotalCVs { get; set; }
+        public int TotalOfertaEmpleos { get; set; }
+        public int TotalOfertaPostulaciones { get; set; }
+        public int TotalPostulaciones { get; set; }
+        public int TotalReclutadores { get; set; }
+    }
+}
