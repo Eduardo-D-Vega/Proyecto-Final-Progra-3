b5603ad44bf1b68c2eaffff0da479cc5f9d7cf2a
Abigail-sys <abigailgaitan3080@gmail.com>
2025-12-09 13:03:46 -0600

    Login y administrador

---
+++ PlataformaEmpleo/Models/Admin.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlataformaEmpleo.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int IdAdmin { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Contrasena { get; set; }
+
+        public string Rol { get; set; } = "Admin";
+    }
+}
+
